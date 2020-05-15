23ecd9be12fa6c41f8ca84097b8e2a9e9c64f83b
tmplk2048 <62500896+tmplk2048@users.noreply.github.com>
2020-05-15 22:21:30 +0800

    Update 1023(4).cpp

--- 1023(4).cpp
+++ 1023(4).cpp
@@ -44,13 +44,90 @@ int main()
 			else if(a[mid]>big&&a[mid+1]>big)
 			{
 				r=mid-1;
+#include<iostream> 
+using namespace std;
+int main()
+{
+	long int n,q;
+	
+	cin>>n>>q;
+	long int a[n];
+	for(int i=0;i<n;i++)
+	{
+		cin>>a[i];
+	}
+	
+	/*for(int i=0;i<n-1;i++)
+	{
+		for(int j=0;j<n-i-1;j++)
+		{
+			if(a[j]>a[j+1])
+			{
+				swap(a[j],a[j+1]);
+			}
+		}
+	}*/
+	
+	
+	
+	while(q--)
+	{
+		long int small,big;
+		cin>>small>>big;
+		if(small>big)
+		{
+			swap(small,big);
+		}
+		
+		int max=0,min=1;
+		int flag1=true,flag2=true;
+		if(a[n-1]<big)
+		{
+			flag1=false;
+			max=n-1;
+		}
+		else if(a[0]>big)
+		{
+			flag1=false;
+			max=-1;
+		}
+		
+		if(a[0]>small)
+		{
+			flag2=false;
+			min=0;
+		}
+		else if(a[n-1]<small)
+		{
+			flag2=false;
+			min=n;
+		}
+		
+		int l=0,r=n;
+		
+		while(l<r-1&&flag1==true)
+		{
+			//cout<<"A";
+			int mid=(l+r)/2;
+			if(a[mid]<=big&&a[mid+1]>big)
+			{
+				max=mid;
+				break;
+			}
+			else if(a[mid]<=big&&a[mid+1]<=big)
+			{
+				l=mid;
+			}
+			else if(a[mid]>big&&a[mid+1]>big)
+			{
+				r=mid;
 			}
 		}
 		
 		l=0;
 		r=n;
 		
-		while(l<r-1)
+		while(l<r-1&&flag2==true)
 		{
 			int mid=(l+r)/2;
 			if(a[mid]>small&&a[mid-1]<=small)
@@ -60,42 +137,17 @@ int main()
 			}
 			else if(a[mid]<=small&&a[mid-1]<=small)
 			{
-				l=mid+1;
+				l=mid;
 			}
 			else if(a[mid]>small&&a[mid-1]>small)
 			{
-				r=mid\-1;
+				r=mid;
 			}
 		}
-		//big
-		if(big>a[n-1])
-		{
-			max=n-1;
-		}
-		/*else if(big==a[n-1])
-		{
-			max=n-2;
-		}*/
-		else if(big<a[0])
-		{
-			max=0;
-		}
-		
-		//small
-		if(small<=a[0])
-		{
-			min=0;
-		}
-		else if(small>a[n-1])
-		{
-			min=n-1;
-		}
-		
-		
-			cout<<max-min+1<<endl;
 		
 		
 		
+		cout<<max-min+1<<endl;
 	}
 	return 0;
 }
