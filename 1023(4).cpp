b4cf5db9e20ae286a6d44a3b3f7582ddf2e1c6c0
tmplk2048 <62500896+tmplk2048@users.noreply.github.com>
2020-05-15 23:24:59 +0800

    Update 1023(4).cpp

--- 1023(4).cpp
+++ 1023(4).cpp
@@ -24,130 +24,52 @@ int main()
 			swap(small,big);
 		}
 		
-		int max=0,min=1;
+			
+		int min=0,max=n-1;//index
+    		int left = 0, right = n - 1;
+   		while (left <= right) {
+    	
+    		int mid = (left + right) / 2;
+        	if (small<=a[mid]&&small>=a[mid-1]){
+        		min=mid;
+        		break;
+			}
+        	else if (small<=a[mid]&&small<=a[mid-1])
+        		right = mid - 1;
+        	else
+        		left = mid + 1;
+       		}
+    
+    	
+    		left = 0;
+		right = n - 1;
+   		while (left <= right) {
+    	
+    		int mid = (left + right) / 2;
+        	if (a[mid]<big&&a[mid+1]>=big)
+    		{
+    			max=mid;
+    			break;
+			}
+        	else if (big<=a[mid])
+        		right = mid - 1;
+        	else
+        		left = mid + 1;
+    	}
 		
-		int l=0,r=n;
-		
-		while(l<r-1)
+		if(big<a[0])
 		{
-			//cout<<"A";
-			int mid=(l+r)/2;
-			if(a[mid]<=big&&a[mid+1]>big)
-			{
-				max=mid;
-				break;
-			}
-			else if(a[mid]<=big&&a[mid+1]<=big)
-			{
-				l=mid+1;
-			}
-			else if(a[mid]>big&&a[mid+1]>big)
-			{
-				r=mid-1;
-#include<iostream> 
-using namespace std;
-int main()
-{
-	long int n,q;
-	
-	cin>>n>>q;
-	long int a[n];
-	for(int i=0;i<n;i++)
-	{
-		cin>>a[i];
-	}
-	
-	/*for(int i=0;i<n-1;i++)
-	{
-		for(int j=0;j<n-i-1;j++)
-		{
-			if(a[j]>a[j+1])
-			{
-				swap(a[j],a[j+1]);
-			}
+			cout<<0<<endl;
 		}
-	}*/
-	
-	
-	
-	while(q--)
-	{
-		long int small,big;
-		cin>>small>>big;
-		if(small>big)
+		else if(small>a[n-1])
 		{
-			swap(small,big);
+			cout<<0<<endl;
 		}
-		
-		int max=0,min=1;
-		int flag1=true,flag2=true;
-		if(a[n-1]<big)
+		else
 		{
-			flag1=false;
-			max=n-1;
+			cout<<max-min+1<<endl;
 		}
-		else if(a[0]>big)
-		{
-			flag1=false;
-			max=-1;
-		}
-		
-		if(a[0]>small)
-		{
-			flag2=false;
-			min=0;
-		}
-		else if(a[n-1]<small)
-		{
-			flag2=false;
-			min=n;
-		}
-		
-		int l=0,r=n;
-		
-		while(l<r-1&&flag1==true)
-		{
-			//cout<<"A";
-			int mid=(l+r)/2;
-			if(a[mid]<=big&&a[mid+1]>big)
-			{
-				max=mid;
-				break;
-			}
-			else if(a[mid]<=big&&a[mid+1]<=big)
-			{
-				l=mid;
-			}
-			else if(a[mid]>big&&a[mid+1]>big)
-			{
-				r=mid;
-			}
-		}
-		
-		l=0;
-		r=n;
-		
-		while(l<r-1&&flag2==true)
-		{
-			int mid=(l+r)/2;
-			if(a[mid]>small&&a[mid-1]<=small)
-			{
-				min=mid;
-				break;
-			}
-			else if(a[mid]<=small&&a[mid-1]<=small)
-			{
-				l=mid;
-			}
-			else if(a[mid]>small&&a[mid-1]>small)
-			{
-				r=mid;
-			}
-		}
-		
-		
-		
-		cout<<max-min+1<<endl;
+		//cout<<min<<" "<<max<<endl;
 	}
 	return 0;
 }
