c5610128d529bd1628c2eeb5932c5bff17f3599c
tmplk2048 <62500896+tmplk2048@users.noreply.github.com>
2020-05-29 18:43:57 +0800

    Add files via upload

---
+++ 689.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<algorithm>
+#include<string>
+using namespace std;
+int main()
+{
+	string dna;
+	while(cin>>dna)
+	{
+		int start=0,end=0;
+		bool flag=false;
+		while(end<dna.length()&&start<dna.length())
+		{
+			start=dna.find("ATG",start);
+			if(start>dna.length())
+			{
+				break;
+			}
+			int arr[3]={};
+			int tag=0,taa=1,tga=2;
+			arr[tag]=dna.find("TAG",start);
+			arr[taa]=dna.find("TAA",start);
+			arr[tga]=dna.find("TGA",start);
+			
+			sort(arr,arr+3);
+			
+			for(int i=0;i<3;i++)
+			{
+				if(arr[i]>0)
+				{
+					end=arr[i];
+					break;
+				}
+			}
+			//cout<<start<<" "<<arr[0]<<" "<<arr[1]<<" "<<arr[2]<<" "<<end<<endl;
+			if(end-start>3&&(end-start)%3==0)
+			{
+				flag=true;
+				for(int i=start+3;i<end;i++)
+				{
+					cout<<dna[i];
+				}
+				cout<<endl;
+			}
+		
+			
+			
+			start+=2;
+		}
+		if(flag==false)
+		{
+			cout<<"No gene\n";
+		}
+		
+	}
+}
