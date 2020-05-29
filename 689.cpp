835bb0eadb99d2eb054d39bf5fa3d33374842ff5
tmplk2048 <62500896+tmplk2048@users.noreply.github.com>
2020-05-29 22:28:11 +0800

    Update 689.cpp

--- 689.cpp
+++ 689.cpp
@@ -13,8 +13,12 @@ int main()
 		bool is_print=false;
 		while(dna.find("ATG",start)>=0&&start<dna.length())
 		{
-			//start=dna.find("ATG",start);
+			start=dna.find("ATG",start);
 			
+			if(start>dna.length())
+			{
+				break;
+			}
 			int arr[3]={};
 			int tag=0,taa=1,tga=2;
 			arr[tag]=dna.find("TAG",start);
@@ -32,7 +36,11 @@ int main()
 				}
 			}
 			//cout<<start<<" "<<arr[0]<<" "<<arr[1]<<" "<<arr[2]<<" "<<end<<endl;
-			if(end-start>3&&(end-start)%3==0)
+			if(dna.find("ATG",start+3)<end||dna.find("TAA",start+3)<end||dna.find("TAG",start+3)<end||dna.find("TGA",start+3)<end)
+			{
+				
+			}
+			else if(end-start>3&&(end-start)%3==0)
 			{
 				is_print=true;
 				for(int i=start+3;i<end;i++)
